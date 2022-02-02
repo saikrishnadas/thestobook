5c971645c7fd22d87584c96892d337480d7cdfe9
Sai Krishna Das <saikrishnadas666@gmail.com>
2022-02-02 17:13:35 +0530

    Fixed styling in main component

--- components/TopMain.tsx
+++ components/TopMain.tsx
@@ -1,15 +1,18 @@
 import TextField from "@mui/material/TextField";
 import styles from "../styles/MainContainer.module.scss";
+import SearchIcon from "@mui/icons-material/Search";
+import SearchBar from "material-ui-search-bar";
 
 function TopMain() {
   return (
     <div className={styles.top__container}>
       <h2>Explore</h2>
-      <TextField
+      <SearchBar
+        placeholder="Search to find a book"
         className={styles.input}
-        id="outlined-search"
-        label="Search to find a book"
-        type="search"
+        // value={searched}
+        // onChange={(searchVal) => requestSearch(searchVal)}
+        // onCancelSearch={() => cancelSearch()}
       />
     </div>
   );
