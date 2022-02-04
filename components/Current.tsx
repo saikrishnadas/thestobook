be9b1074b2ced84c3888335ea4511c5d6658747d
Sai Krishna Das <saikrishnadas666@gmail.com>
2022-02-04 10:11:19 +0530

    Adding Reading Page
    Added Mini Profile Container and Reading Page

--- components/Current.tsx
+++ components/Current.tsx
@@ -1,19 +1,22 @@
 import styles from "../styles/NavContainer.module.scss";
+import Link from "next/link";
 
 function Current() {
   return (
     <div className={styles.current__container}>
       <h3>Current Reading Book</h3>
-      <div className={styles.current__reading}>
-        <img
-          src="https://images-na.ssl-images-amazon.com/images/I/419Lr6CbZ7L._SX323_BO1,204,203,200_.jpg"
-          alt="bookimg"
-        />
-        <div>
-          <p>The girl in room 105</p>
-          <p>Chetan Bagat</p>
+      <Link href="reading">
+        <div className={styles.current__reading}>
+          <img
+            src="https://images-na.ssl-images-amazon.com/images/I/419Lr6CbZ7L._SX323_BO1,204,203,200_.jpg"
+            alt="bookimg"
+          />
+          <div>
+            <p>The girl in room 105</p>
+            <p>Chetan Bagat</p>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
---
+++ pages/reading/index.tsx
@@ -0,0 +1,82 @@
+import styles from "../../styles/Reading.module.scss";
+import rightIcon from "../../public/Group 5.png";
+import leftIcon from "../../public/Group 6.png";
+
+function index() {
+  return (
+    <div className={styles.reading__container}>
+      <div className={styles.profile__division}>
+        <img
+          src="https://miro.medium.com/fit/c/1360/1360/1*prGxEhjvfMxpeagLYWxA4w.jpeg"
+          alt="sai"
+        />
+        <h3>SAI KRISHNA DAS</h3>
+        <p>Premium Member</p>
+      </div>
+    
+      <div className={styles.paper__container}>
+        <div className={styles.heading__container}>
+          <p>Chapter 1</p>
+          <p>1/257</p>
+        </div>
+        <div className={styles.content__container}>
+          <p>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using 'Content here, content
+            here', making it look like readable English. Many desktop publishing
+            packages and web page editors now use Lorem Ipsum as their default
+            model text, and a search for 'lorem ipsum' will uncover many web
+            sites still in their infancy. Various versions have evolved over the
+            years, sometimes by accident, sometimes on purpose (injected humour
+            and the like). It is a long established fact that a reader will be
+            distracted by the readable content of a page when looking at its
+            layout. The point of using Lorem Ipsum is that it has a more-or-less
+            normal distribution of letters, as opposed to using 'Content here,
+            content here', making it look like readable English. Many desktop
+            publishing packages and web page editors now use Lorem Ipsum as
+            their default model text, and a search for 'lorem ipsum' will
+            uncover many web sites still in their infancy. Various versions have
+            evolved over the years, sometimes by accident, sometimes on purpose
+            (injected humour and the like). It is a long established fact that a
+            reader will be distracted by the readable content of a page when
+            looking at its layout. The point of using Lorem Ipsum is that it has
+            a more-or-less normal distribution of letters, as opposed to using
+            'Content here, content here', making it look like readable English.
+            Many desktop publishing packages and web page editors now use Lorem
+            Ipsum as their default model text, and a search for 'lorem ipsum'
+            will uncover many web sites still in their infancy. Various versions
+            have evolved over the years, sometimes by accident, sometimes on
+            purpose (injected humour and the like). It is a long established
+            fact that a reader will be distracted by the readable content of a
+            page when looking at its layout. The point of using Lorem Ipsum is
+            that it has a more-or-less normal distribution of letters, as
+            opposed to using 'Content here, content here', making it look like
+            readable English. Many desktop publishing packages and web page
+            editors now use Lorem Ipsum as their default model text, and a
+            search for 'lorem ipsum' will uncover many web sites still in their
+            infancy. Various versions have evolved over the years, sometimes by
+            accident, sometimes on purpose (injected humour and the like). It is
+            a long established fact that a reader will be distracted by the
+            readable content of a page when looking at its layout. The point of
+            using Lorem Ipsum is that it has a more-or-less normal distribution
+            of letters, as opposed to using 'Content here, content here', making
+            it look like readable English. Many desktop publishing packages and
+            web page editors now use Lorem Ipsum as their default model text,
+            and a search for 'lorem ipsum' will uncover many web sites still in
+            their infancy. Various versions have evolved over the years,
+            sometimes by accident, sometimes on purpose (injected humour and the
+            like).
+          </p>
+        </div>
+      </div>
+      <div className={styles.arrow__container}>
+        <img src={leftIcon.src} alt="" />
+        <img src={rightIcon.src} alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default index;
